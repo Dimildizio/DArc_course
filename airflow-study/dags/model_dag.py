7d24a9c9101c7bb95932d9aa98ca92a91050e3c5
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-10-31 05:54:21 +0800

    feat(airflow): add DAGs for mlflow and model run

--- airflow-study/dags/model_dag.py
+++ airflow-study/dags/model_dag.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from main import run_model, preprocess_for_model
+from main import preprocess_for_model, save_train_test, train_model, save_report, predict
 
 
 default_args = {
@@ -16,7 +16,7 @@ default_args = {
 dag = DAG(
     'mlflow_dag',
     default_args=default_args,
-    description='A DAG for MLflow',
+    description='A DAG to train and test the model',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 10, 26),
     catchup=False
@@ -28,17 +28,35 @@ with dag:
         python_callable=preprocess_for_model,
         op_args=['mydb.db', 'SELECT * FROM DWH_DATAMART'],
     )
-    run_model = PythonOperator(
-        task_id='train_mlflow_model',
-        python_callable=run_model,
-        op_args=['mydb.db', 'SELECT * FROM DWH_DATAMART'],
+
+    split_data = PythonOperator(
+        task_id='split_data_train_test',
+        python_callable=save_train_test,
+        op_args=['ready_data.csv'],
     )
 
-    '''
-    log_mlflow_params = PythonOperator(
-        task_id='log_mlflow_params',
-        python_callable=log_mlflow_parameters, #To be implemented
-        op_args=[datamart_task.output],  
-    )'''
+    train_save = PythonOperator(
+        task_id='train_and_save_model',
+        python_callable=train_model,
+        op_args=['train_data.csv'],
+    )
 
-    train_mlflow_model #>> log_mlflow_params
+    save_train_metrics = PythonOperator(
+        task_id='log_train_metrics',
+        python_callable=save_report,
+    )
+
+    test_save = PythonOperator(
+        task_id='predict_test_data',
+        python_callable=predict,
+        op_args=['test_data.csv'],
+    )
+
+
+    save_test_metrics = PythonOperator(
+        task_id='log_test_rewrite_metrics_db',
+        python_callable=save_report,
+    )
+
+
+    preprocess_data >> split_data >> train_save >> save_train_metrics >> test_save >> save_test_metrics
