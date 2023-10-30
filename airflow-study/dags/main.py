17bc09a5fdedfcf3a2ed143efc97de30ac66ce28
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-10-31 05:41:56 +0800

    refactor(mlflow): refactor, annotate and clean up the code

--- airflow-study/dags/main.py
+++ airflow-study/dags/main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import requests, os
+import requests
 import logging
 from py_scripts.xlsx2pd import get_df
 from py_scripts.filter_db import *
@@ -9,12 +8,14 @@ from py_model.model import MyModel, model_predict, save_pkl, load_pkl
 from py_model.preprocessing import preprocess_datamart, get_traintest
 import openpyxl
 
+
 def download_file(url):
     response = requests.get(url)
     print("Downloading")
     with open('input_data_.xlsx', 'wb') as file:
         file.write(response.content)
     print('downloaded')
+
 
 def parse_excel_sheet(sheet_name):
     print(os.listdir())
@@ -30,6 +31,7 @@ def create_db(db_name):
     logging.info("create db")
     if_db(db_name)
     logging.info("db created")
+
 
 def create_stg(file_path):
     logging.info("Starting creating stg")
@@ -48,94 +50,82 @@ def create_stg(file_path):
     logging.info('Writing STGS')
     wrapper_con('mydb.db', create_stgs, df_transactions, df_cd, df_ncl, df_customer_addresses)
 
+
 def create_dwh(db_filename, scripts_folder):
     logging.info("creating DWHS")
     # Create DWH tables
     create_scripts = getpath(scripts_folder, ('create_DWH_FACT_TRANSACTIONS.sql',
-                                             'create_DWH_DIM_CUSTOMER_ADDRESSES.sql',
-                                             'create_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
+                                              'create_DWH_DIM_CUSTOMER_ADDRESSES.sql',
+                                              'create_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
     wrapper_cursor(db_filename, read_scripts, create_scripts, many=True)
+
 
 def insert2dwh(db_filename, scripts_folder):
     logging.info("writing DWHS")
     insert_scripts = getpath(scripts_folder, ('insert_DWH_FACT_TRANSACTIONS.sql',
-                                             'insert_DWH_DIM_CUSTOMER_ADDRESSES.sql',
-                                             'insert_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
+                                              'insert_DWH_DIM_CUSTOMER_ADDRESSES.sql',
+                                              'insert_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
     wrapper_cursor(db_filename, read_scripts, insert_scripts, many=False)
 
 
 def produce_datamart(db_filename, scripts_folder):
     logging.info("writing DWHS")
     insert_scripts = getpath(scripts_folder, ('insert_DWH_FACT_TRANSACTIONS.sql',
-                                             'insert_DWH_DIM_CUSTOMER_ADDRESSES.sql',
-                                             'insert_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
+                                              'insert_DWH_DIM_CUSTOMER_ADDRESSES.sql',
+                                              'insert_DWH_DIM_CUSTOMER_DEMOGRAPHIC.sql'))
     wrapper_cursor(db_filename, read_scripts, insert_scripts, many=False)
-
-
-def preprocess_for_model(db_loc, query):
-        df = read_to_pd(db_loc, query)
-        print(df['order_status'].value_counts())
-        df = preprocess_datamart(df, 'order_status_Cancelled')
-        logging.info('saving data for model')
-        df.to_csv('ready_data.csv', sep=',')
-
-
-
-def save_train_test(name='ready_data.csv', target='order_status_Cancelled'):
-    df_train, df_test = get_traintest(name, target)
-    df_test.to_csv('test_data.csv', index=False)
-    df_train.to_csv('train_data.csv', index=False)
-
-
-
-def predict_from_data(name='test_data.csv', target='order_status_Cancelled'):
-    df = pd.read_csv(name)
-    X = df.drop(target, axis=1)
-    y = df[target]
-    model = load_model()
-    result = model_predict(X,y)
-    report_df = pd.DataFrame(result).transpose()
-    return report_df
 
 
 def logparams():
     mlflow.log_param("n_estimators", 100)
     mlflow.log_param("max_depth", 5)
     mlflow.log_param("random_state", 42)
-def logmetrics(report):
-    mlflow.log_metric("precision", report['0']['precision'])
-    mlflow.log_metric("recall", report['0']['recall'])
-    mlflow.log_metric("f1", report['0']['f1-score'])
 
-def savemodel(model):
-    save_pkl(model)
-    model_name = 'OrderStatusModel'
-    mlflow.sklearn.log_model(model.model, "rf_model")
-    model_uri = mlflow.sklearn.get_model_uri("rf_model")
-    mlflow.register_model(model_uri, model_name)
-    model = mlflow.registered_model.get_model_version(name=model_name, version=1)
-    mlflow.registered_model.transition_model_version_stage(name=model_name, version=model.version,stage="Production")
+
+def logmetrics(report):
+    with mlflow.start_run() as run:
+        mlflow.log_metric("precision", report['0']['precision'])
+        mlflow.log_metric("recall", report['0']['recall'])
+        mlflow.log_metric("f1", report['0']['f1-score'])
+
+
+def preprocess_for_model(db_loc, query):
+    """"
+    Step 1: preprocess data for model
+    """
+    df = read_to_pd(db_loc, query)
+    df = preprocess_datamart(df, 'order_status_Cancelled')
+    logging.info('saving data for model')
+    df.to_csv('ready_data.csv', sep=',')
+
+
+def save_train_test(name='ready_data.csv', target='order_status_Cancelled'):
+    """
+    Step 2: save train test data
+    """
+    df_train, df_test = get_traintest(name, target)
+    df_test.to_csv('test_data.csv', index=False)
+    df_train.to_csv('train_data.csv', index=False)
+
 
 def train_model(name='train_data.csv'):
-    df = pd.read_csv(name)
-    model = MyModel()
-    logparams()
-    model.mock_mainloop(df, target='order_status_Cancelled')
-    report = predict_from_data(name)
-    return report
+    """
+    Step 3: Train model
+    """
+    with mlflow.start_run() as run:
+        df = pd.read_csv(name)
+        model = MyModel()
+        logparams()
+        model.mock_mainloop(df, target='order_status_Cancelled')
+        report = predict(name)
+        savemodel(model)
+        return report
 
-def load_model():
-    model_uri = "models:/OrderStatusModel/Production"
-    model = mlflow.sklearn.load_model(model_uri=model_uri)
-    return model
 
-
-def test_model(name='test_data.csv'):
-    # Need to show metrics logging separately
-    report = predict_from_data(name)
-    # So we again perform the atrocities like below
-    return report
 def save_report(report):
+    """
+    Steps 4 and 7: Log all metrics and load them to SQL
+    """
     name = 'report.pkl'
     save_pkl(report, name)
     log_all_metrics(name)
@@ -143,21 +133,59 @@ def save_report(report):
 
 def log_all_metrics(name):
     report = load_pkl(name)
-    logmetrics(report)
-    log_metrics_to_sql(report)
+    with mlflow.start_run() as run:
+        logmetrics(report)
+        log_metrics_to_sql(report)
     logging.info(report)
     print(report)
 
 
+def savemodel(model):
+    """
+    Step 5: Save model to Model Registry
+    """
+    save_pkl(model)
+    with mlflow.start_run() as run:
+        model_name = 'OrderStatusModel'
+        mlflow.sklearn.log_model(model.model, "rf_model")
+        model_uri = mlflow.sklearn.get_model_uri("rf_model")
+        mlflow.register_model(model_uri, model_name)
+        model = mlflow.registered_model.get_model_version(name=model_name, version=1)
+        mlflow.registered_model.transition_model_version_stage(name=model_name, version=model.version, stage="Production")
+
+
+def load_model(model_uri="models:/OrderStatusModel/Production"):
+    model = mlflow.sklearn.load_model(model_uri=model_uri)
+    return model
+
+
+def predict(name='test_data.csv', target='order_status_Cancelled'):
+    """
+    Step 6: Predict test data
+    """
+    df = pd.read_csv(name)
+    X = df.drop(target, axis=1)
+    y = df[target]
+
+    with mlflow.start_run() as run:
+        load_model()
+        result = model_predict(X, y)
+    report_df = pd.DataFrame(result).transpose()
+    return report_df
+
+
 def run_model(db_name, query):
-    #mock func for airflow and mlflow
-    # with mlflow.start_run() as run:
-    preprocess_for_model(db_name, query)
-    save_train_test()
-    train_report = train_model()
-    save_report(train_report)
-    test_report = test_model()
-    save_report(test_report)
+    """
+    mock func for airflow and mlflow.
+    # each call here emulates a task in  DAGs
+    """
+    with mlflow.start_run() as run:
+        preprocess_for_model(db_name, query)
+        save_train_test()
+        train_report = train_model()
+        # savemodel()
+        save_report(train_report)
+        test_report = predict()
+        save_report(test_report)
 
-
-#run_model('mydb1.db', 'SELECT * FROM DWH_DATAMART')
+# run_model('mydb1.db', 'SELECT * FROM DWH_DATAMART')
